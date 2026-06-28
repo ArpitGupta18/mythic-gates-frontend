274bcb49c91daca2f6bd41a6a146be51e7afc408
Arpit Gupta <gupta.14arpit@gmail.com>
2026-06-28 14:39:06 +0545

    feat: home page to include start and continue battle

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth } from '../../../core/services/auth';
 import { BattleService } from '../../../core/services/battle.service';
+import { BattleResponse } from '../../../core/models/battle/battle-response';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -13,16 +14,39 @@ export class Home {
   router = inject(Router);
   private battleService = inject(BattleService);
 
-  playGame() {
+  ongoingBattle = signal<BattleResponse | null>(null);
+  loadingBattle = signal(false);
+
+  ngOnInit() {
+    this.loadOnGoingBattle();
+  }
+
+  loadOnGoingBattle() {
+    this.loadingBattle.set(true);
+
     this.battleService.getOngoingBattle().subscribe({
       next: (response) => {
-        this.router.navigate(['game/battles', response.data!.battleId]);
+        this.ongoingBattle.set(response.data);
+        this.loadingBattle.set(false);
       },
-      error: (err) => {
-        if (err.status === 404) {
-          this.router.navigate(['game/battle/start/characters']);
-        }
+      error: () => {
+        this.ongoingBattle.set(null);
+        this.loadingBattle.set(false);
       },
     });
+  }
+
+  startNewBattle() {
+    if (this.ongoingBattle()) return;
+
+    this.router.navigate(['game/battle/start/characters']);
+  }
+
+  continueBattle() {
+    const battle = this.ongoingBattle();
+
+    if (!battle) return;
+
+    this.router.navigate(['game/battles', battle.battleId]);
   }
 }
